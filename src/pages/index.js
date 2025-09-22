a1deb3796eae6d7cf13ca5b24440b1fa21690d6b
surya chawan <suryachawan112200@gmail.com>
2025-09-22 20:20:44 +0530

    Force correct file casing for PricingCard

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import PricingCard from "../components/pricingcard";
+import PricingCard from "../components/PricingCard";
 import UploadBox from "../components/UploadBox";
 import { useRouter } from "next/router";
 import { motion } from "framer-motion";
